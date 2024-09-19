c1ec4806b3ff271229db576df4c44fe0fef253c1
SevenLee <83711511+ThanhT7@users.noreply.github.com>
2024-09-20 01:11:37 +0700

    Xong phần khai báo

---
+++ W01Ex16/src/firstDegree.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class firstDegree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number a: ");
+        int numBerA = scanner.nextInt();
+        System.out.println("Enter the number b: ");
+        int numBerB = scanner.nextInt();
+        int x;
+        
+    }
+}
