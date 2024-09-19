51f8acb2a1cc00894d1d5ee459ce043ecd047d89
SevenLee <83711511+ThanhT7@users.noreply.github.com>
2024-09-20 00:43:36 +0700

    H mới đưa sang file sumEvenOdd hehe

--- W01Ex14/src/Main.java
+++ W01Ex14/src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
+import java.util.Scanner; // Import the Scanner class
+public class sumEvenOdd {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in); // Create a Scanner instance
+        System.out.println("Enter the number: ");
+        int aNumber = scanner.nextInt();
+        int sum;
+        int count;
+        if (aNumber % 2 == 0){
+            count = (aNumber - 2)/2 +1;
+            sum = (aNumber +2) * count / 2;
+            System.out.println("Even sum: " + sum);
+        }
+        else {
+            count = (aNumber - 1)/2 +1;
+            sum = (aNumber +1) * count / 2;
+            System.out.println("Odd sum: " + sum);
+        }
     }
 }
