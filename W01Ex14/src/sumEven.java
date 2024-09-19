95eec8001770b1fc89c65ab1dc51f55850bedb42
SevenLee <83711511+ThanhT7@users.noreply.github.com>
2024-09-20 00:49:04 +0700

    Complete Even Sum or Odd sum

---
+++ W01Ex14/src/sumEven.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; // Import the Scanner class
+public class sumEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Create a Scanner instance
+        System.out.println("Enter the number: ");
+        int aNumber = scanner.nextInt();
+        int sum;
+        int count;
+        if (aNumber % 2 == 0){
+            count = (aNumber - 2)/2 +1;
+            sum = (aNumber +2) * count / 2;
+            System.out.println("Even sum: " + sum);
+        }
+        else {
+            count = (aNumber - 1)/2 +1;
+            sum = (aNumber +1) * count / 2;
+            System.out.println("Odd sum: " + sum);
+        }
+    }
+}
