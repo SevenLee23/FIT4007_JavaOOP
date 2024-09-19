cfae53ccba6d3730618f1f8f8bee6cccdb8e00cb
SevenLee <83711511+ThanhT7@users.noreply.github.com>
2024-09-20 01:15:00 +0700

    Xong rồi này

--- W01Ex16/src/firstDegree.java
+++ W01Ex16/src/firstDegree.java
@@ -3,10 +3,17 @@ public class firstDegree {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number a: ");
-        int numBerA = scanner.nextInt();
+        float numBerA = scanner.nextFloat();
         System.out.println("Enter the number b: ");
-        int numBerB = scanner.nextInt();
-        int x;
-        
+        float numBerB = scanner.nextFloat();
+        float x;
+        if (numBerA == 0){
+            System.out.println("Infinity");
+        }
+        else {
+            x = - numBerB / numBerA;
+            System.out.println("X = " + x);
+        }
+
     }
 }
