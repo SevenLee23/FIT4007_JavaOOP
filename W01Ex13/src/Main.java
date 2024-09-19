5113c968adfbb8436b780ae646a249c388dbfb00
SevenLee <83711511+ThanhT7@users.noreply.github.com>
2024-09-20 00:07:09 +0700

    Đang bị lỗi class

--- W01Ex13/src/Main.java
+++ W01Ex13/src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
+import java.util.Scanner; // Import the Scanner class
+public class SampleClass {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in); // Create a Scanner instance
+        System.out.println("Enter a number: ");
+        int aNumber = scanner.nextInt(); // Read user input
+        int sum = (aNumber+1)*aNumber/2;
+        System.out.println("Sum=  " + sum);
     }
 }
